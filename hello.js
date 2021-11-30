5f4d7b80e788c2048b31ed857222a76c39979f39
buianhduc <buianhduc.popodoo2@gmail.com>
2021-11-30 21:46:32 +0700

    edit hello.js

--- hello.js
+++ hello.js
@@ -2,4 +2,8 @@ console.log("hello world");
 
 console.log("git training");
 
+<<<<<<< Updated upstream
 console.log("never gonna give you up");
+=======
+console.log("1 2 3 4");
+>>>>>>> Stashed changes
