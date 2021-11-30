05d09f3fb50ad2bb7ecd228a1e4907cd93c50741
buianhduc <buianhduc.popodoo2@gmail.com>
2021-11-30 22:16:08 +0700

    add loop function

--- hello.js
+++ hello.js
@@ -2,8 +2,9 @@ console.log("hello world");
 
 console.log("git training");
 
-<<<<<<< Updated upstream
 console.log("never gonna give you up");
-=======
 console.log("1 2 3 4");
->>>>>>> Stashed changes
+
+for(var i = 0; i < 10; i+=1){
+    console.log(i);
+}
